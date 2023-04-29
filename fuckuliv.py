5f5deb03da84b1aafe3a9c85a5fe19ab12f1a711
Qutaiba Al-Nuaimy <qan205@nyu.edu>
2023-04-29 18:00:23 +0400

    ship it

--- fuckuliv.py
+++ fuckuliv.py
@@ -5,27 +5,45 @@ import re
 import sys
 import os
 import random
+import time
+from tqdm import tqdm
+from colorama import Fore, Style, init
 
-# Define the character set and number set
-char_set = 'QUTAILNMRY'
-num_set = '0129'
+def clear_terminal():
+    print('\033[2J\033[H', end='')
 
-def check_password(password, attempt_num):
-    try:
-        with open('encrypted.pdf', 'rb') as pdf_file:
-            pdfReader = PyPDF2.PdfReader(pdf_file)
-            if pdfReader.decrypt(password) == 1:
-                print(f'Password found in attempt {attempt_num}: {password}')
-                return True
-            else:
-                sys.stdout.write(f'\rAttempt {attempt_num}: {password}')
-                sys.stdout.flush()
-                return False
-    except Exception as e:
-        print(f'Error occurred in attempt {attempt_num} with password {password}: {e}')
-        return False
 
-def generate_passwords():
+def print_banner():
+    banner = f'''
+{Fore.CYAN}========================================
+           PDF Password Cracker
+========================================{Style.RESET_ALL}
+'''
+    print(banner)
+
+
+def sanitize_input(prompt, pattern, error_msg):
+    while True:
+        user_input = input(prompt)
+        if re.match(pattern, user_input):
+            return user_input
+        else:
+            print(error_msg)
+
+
+def get_user_inputs():
+    clear_terminal()
+    print_banner()
+
+    char_set = sanitize_input("Enter the character set (uppercase letters only): ",
+                              "^[A-Z]+$", "Invalid input. Please enter uppercase letters only.")
+    num_set = sanitize_input("Enter the number set (digits only): ",
+                             "^[0-9]+$", "Invalid input. Please enter digits only.")
+    filepath = input("Enter the PDF file path: ")
+    return char_set, num_set, filepath
+
+
+def generate_passwords(char_set, num_set):
     """Generate all possible passwords that match the regex."""
     passwords = []
     for i in range(1, 5):
@@ -39,33 +57,39 @@ def generate_passwords():
     random.shuffle(passwords)
     return passwords
 
-if __name__ == '__main__':
 
-    if not os.path.exists('passwords.txt'):
-        with open('passwords.txt', 'w'):
-            pass
-
-    # Load previously tried passwords
-    tried_passwords = set()
-    with open('passwords.txt', 'r') as f:
-        for line in f:
-            tried_passwords.add(line.strip())
-
-    # Brute force attack with parallelism
-    attempt_num = len(tried_passwords)
-    found_password = None
-    with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
-        futures = {executor.submit(check_password, password, attempt_num + i + 1): password for i, password in enumerate(generate_passwords()) if password not in tried_passwords}
-        for future in concurrent.futures.as_completed(futures):
-            password = futures[future]
-            if future.result():
-                found_password = password
-                break
+def check_password(pdf_filepath, password, start_time):
+    try:
+        with open(pdf_filepath, 'rb') as pdf_file:
+            pdfReader = PyPDF2.PdfReader(pdf_file)
+            if pdfReader.decrypt(password) == 1:
+                return password, time.time() - start_time
             else:
-                with open('passwords.txt', 'a') as f:
-                    f.write(f'{password}\n')
+                return None, time.time() - start_time
+    except Exception as e:
+        print(f'Error occurred with password {password}: {e}')
+        return None, time.time() - start_time
 
-    if found_password is not None:
-        print(f'\nPassword found in attempt {attempt_num}: {found_password}')
-    else:
-        print(f'\nAll possible passwords tried, giving up')
+if __name__ == '__main__':
+    init(autoreset=True)  # Initialize colorama
+    char_set, num_set, filepath = get_user_inputs()
+    clear_terminal()  # Clear inputs once cracking starts
+
+    passwords = generate_passwords(char_set, num_set)
+    start_time = time.time()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        future_to_password = {executor.submit(check_password, filepath, password, start_time): password for password in passwords}
+        attempt_num = 0
+
+        for future in concurrent.futures.as_completed(future_to_password):
+            password = future_to_password[future]
+            attempt_num += 1
+            decrypted_password, elapsed_time = future.result()
+
+            if decrypted_password is not None:
+                print(f'\n{Fore.GREEN}Password found: {decrypted_password}{Style.RESET_ALL}')
+                break
+
+            sys.stdout.write(f'\r{Fore.YELLOW}Attempt {attempt_num}: {password}, Elapsed time: {elapsed_time:.2f} seconds{Style.RESET_ALL}')
+            sys.stdout.flush()
